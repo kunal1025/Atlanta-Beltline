f5b33686309ff3c8b984535cdb6dd4f9a3e892de
Kunal Patel <kunalp1025@gmail.com>
2019-04-19 18:46:29 -0400

    add take transit

--- transit.py
+++ transit.py
@@ -5,6 +5,41 @@ from flask import (
 import db
 
 bp = Blueprint('transit', __name__, url_prefix='/transit')
+
+@bp.route('/take', methods=('GET', 'POST'))
+def take():
+    conn = db.get_connection()
+    error = None
+    if request.method == 'GET':
+        site = request.args.get('site')
+        transport_type = request.args.get('type')
+        low_price = request.args.get('lowPrice')
+        high_price = request.args.get('highPrice')
+        transitSQL = 'select t.transitType as TransitType, t.transitRoute as TransitRoute, t.Price, count(*) as cs from transit t join connect c where t.TransitType = c.TransitType AND t.TransitRoute = c.TransitRoute group by t.TransitType, t.TransitRoute'
+        siteSQL = 'select name from site'
+        with conn.cursor() as cursor:
+            cursor.execute(transitSQL)
+            transits = cursor.fetchall()
+            cursor.execute(siteSQL)
+            sites = cursor.fetchall()
+            print('sites:/n')
+            print(sites)
+        return render_template('/transit/takeTransit.html', transits=transits, sites=sites)
+    elif request.method == 'POST':
+        username = session['username']
+        transit = request.form.get('transit').split(',')
+        date = request.form.get('date')
+        take_transit = 'select username from take where username = %s AND TransitDate = %s AND TransitRoute = %s AND TransitType = %s'
+        with conn.cursor() as cursor:
+            cursor.execute(take_transit, (username, date, transit[0], transit[1]))
+            result = cursor.fetchone()
+            if (result):
+                return redirect('/transit/take')
+            else:
+                #insert into table
+                return redirect('/transit/take')
+        return redirect('/transit/take')
+
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
     conn = db.get_connection()
