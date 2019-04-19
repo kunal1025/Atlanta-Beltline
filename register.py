6b38e7761f0598483e67fca1c2085fd094357441
Patel, Parth K <ppatel396@gatech.edu>
2019-04-19 19:39:09 -0400

    finished register employee

--- register.py
+++ register.py
@@ -30,8 +30,11 @@ def register_user():
         try:
             with conn.cursor() as cursor:
                 insertUser = 'INSERT INTO beltline.user (%s, %s, ‘Pending’, %s, %s)'
+                insertVisitor = 'INSERT INTO beltline.visitor (%s)'
                 insertEmail = 'INSERT INTO beltline.email (%s, %s)'
                 cursor.execute(insertUser, (username, hashed_password, first_name, last_name))
+                cursor.commit()
+                cursor.execute(insertVisitor, (username))
                 cursor.commit()
                 for email in emails:
                     cursor.execute(insertEmail, (username, email))
@@ -61,7 +64,7 @@ def register_visitor():
         try:
             with conn.cursor() as cursor:
                 insertUser = 'INSERT INTO beltline.user (%s, %s, ‘Pending’, %s, %s)'
-                insertVisitor = 'INSERT Into beltline.visitor (%s)'
+                insertVisitor = 'INSERT INTO beltline.visitor (%s)'
                 insertEmail = 'INSERT INTO beltline.email (%s, %s)'
                 cursor.execute(insertUser, (username, hashed_password, first_name, last_name))
                 cursor.commit()
@@ -83,6 +86,50 @@ def register_employee():
     if request.method == 'GET':
         return render_template('/auth/register_employee.html')
     else:
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        emails = request.form.getlist('email[]')
+        usertype = request.form.get('type')
+        employeeID = request.form.get('employeeID')
+        phone = request.form.get('phone')
+        address = request.form.get('address')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zipcode = request.form.get('zipcode')
+
+        hashed_password = generate_password_hash(password)
+
+        conn = db.get_connection()
+        try:
+            with conn.cursor() as cursor:
+                insertUser = 'INSERT INTO beltline.user (%s, %s, ‘Pending’, %s, %s)'
+                insertVisitor = 'INSERT Into beltline.visitor (%s)'
+                insertEmail = 'INSERT INTO beltline.email (%s, %s)'
+                insertEmployee = 'INSERT INTO beltline.employee (%s, %s, %s, %s, %s, %s, %s)'
+                if usertype == 'manager':
+                    insertManager = 'INSERT INTO beltline.manager (%s)'
+                else:
+                    insertStaff = 'INSERT INTO beltline.staff (%s)'
+                cursor.execute(insertUser, (username, hashed_password, first_name, last_name))
+                cursor.commit()
+                cursor.execute(insertEmployee, (username, employeeID, phone, address, city, state, zipcode))
+                cursor.commit()
+                cursor.execute(insertManager, (username))
+                cursor.commit()
+                cursor.execute(insertStaff, (username))
+                cursor.commit()
+                cursor.execute(insertVisitor, username)
+                cursor.commit()
+                for email in emails:
+                    cursor.execute(insertEmail, (username, email))
+                cursor.commit()
+        except Exception as e:
+            print(e)
+            #return render_template('/error/500.html')
+            return redirect('/register/visitor')
+
         return redirect('/login')
 
 @bp.route('/employee-visitor', methods=['GET', 'POST'])
