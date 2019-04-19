1cff829d6ef320f93cfb6c36f83480eccaa8907b
Patel, Parth K <ppatel396@gatech.edu>
2019-04-19 18:38:34 -0400

    added register visitor

--- register.py
+++ register.py
@@ -47,8 +47,35 @@ def register_user():
 @bp.route('/visitor', methods=['GET', 'POST'])
 def register_visitor():
     if request.method == 'GET':
-        return render_template('/auth/register_visitor.html')
+        return render_template('/register/manage/register_visitor.html')
     else:
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        emails = request.form.getlist('email[]')
+
+        hashed_password = generate_password_hash(password)
+
+        conn = db.get_connection()
+        try:
+            with conn.cursor() as cursor:
+                insertUser = 'INSERT INTO beltline.user (%s, %s, ‘Pending’, %s, %s)'
+                insertVisitor = 'INSERT Into beltline.visitor (%s)'
+                insertEmail = 'INSERT INTO beltline.email (%s, %s)'
+                cursor.execute(insertUser, (username, hashed_password, first_name, last_name))
+                cursor.commit()
+                cursor.execute(insertVisitor, username)
+                cursor.commit()
+                for email in emails:
+                    cursor.execute(insertEmail, (username, email))
+                cursor.commit()
+        except Exception as e:
+            print(e)
+            #return render_template('/error/500.html')
+            return redirect('/register/visitor')
+
+
         return redirect('/login')
 
 @bp.route('/employee', methods=['GET', 'POST'])
