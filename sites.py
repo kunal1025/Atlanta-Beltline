970c269bc4bb2c753f4ae01b138abaa52ea2ae93
Patel, Deep P <dpatel371@gatech.edu>
2019-04-20 19:39:21 -0400

    View History is done but I put it in site since I realized that we need to connect the py files

--- sites.py
+++ sites.py
@@ -3,6 +3,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for)
 import db
 
+def ifnull(var,value):
+    if var == '' or var is None:
+        return value
+    else:
+        return var.replace("'", "")
+
 bp = Blueprint('sites', __name__, url_prefix='/site')
 @bp.route('/edit/<SiteName>', methods=['GET', 'POST'])
 # THE HTML IS INCORRECT
@@ -51,8 +57,6 @@ def create(SiteName):
             cursor.execute(manager_drop_down)
 
             return render_template('site/edit_site.html', data=info)
-            
-
     else:
         name = request.form.get('name')
         zip_code = request.form.get("zipcode")
@@ -82,3 +86,29 @@ def detail(Name):
             sitedate = cursor.fetchone()
 
     return redirect('sites/site_detail.html', data=sites)
+
+@bp.route('/history', methods=('GET',))
+def history():
+    conn = db.get_connection()
+    if request.method == 'GET':
+        with conn.cursor() as cursor:
+            username = session["username"]
+            startdate = request.form.get("startdate")
+            enddate = request.form.get("enddate")
+            eventname = request.form.get("event")
+
+            visit_history = "(SELECT VisitEventDate AS date, VisitEventName AS event, Event.SiteName AS site, Price from visit_event join event ON event.Name = visit_event.VisitEventName AND event.SiteName = visit_event.SiteName AND event.StartDate = visit_event.StartDate WHERE username = %s AND event.StartDate >= %s AND event.EndDate <= %s AND event.Name = %s) UNION (SELECT Date, '' AS VisitEventName, Name AS SiteName, '0' AS Price from site join visit_site on visit_site.SiteName = site.Name WHERE username = %s)"
+
+            cursor.execute(visit_history, (username, ifnull(startdate, "%"), ifnull(enddate, "%"), ifnull(eventname, "%"), username))
+            visit_history = cursor.fetchall()
+
+            sites = "SELECT site.Name from site"
+            cursor.execute(sites)
+            sites = cursor.fetchall()
+
+        return render_template('visit_history.html', data=visit_history, site=sites)
+
+
+
+
+
--- visits.py
+++ visits.py
@@ -2,14 +2,27 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for)
 import db
-bp = Blueprint('transit', __name__, url_prefix='/visit')
-@bp.route('/edit/<SiteName>', methods=['GET', 'POST'])
+
+bp = Blueprint('visits', __name__, url_prefix='/visit')
+@bp.route('/history', methods=('GET',))
 def history():
     conn = db.get_connection()
     if request.method == 'GET':
         with conn.cursor() as cursor:
-            visit_history = "(SELECT VisitEventDate AS date, VisitEventName AS event, Event.SiteName AS site, Price from visit_event join event "\
-            "ON event.Name = visit_event.VisitEventName AND event.SiteName = visit_event.SiteName AND event.StartDate = visit_event.StartDate WHERE username = %s) "\
-            "UNION (SELECT Date, "" AS VisitEventName, Name AS SiteName, "0" AS Price from site join visit_site on visit_site.SiteName = site.Name) "\
-            "WHERE username = %s"
-            cursor.execute(visit_history, )
+            username = session['username']
+            startdate = request.form.get("startdate")
+            enddate = request.form.get("enddate")
+            eventname = request.form.get("event")
+
+            visit_history = "(SELECT VisitEventDate AS date, VisitEventName AS event, Event.SiteName AS site, Price from visit_event join event " \
+            "ON event.Name = visit_event.VisitEventName AND event.SiteName = visit_event.SiteName AND event.StartDate = visit_event.StartDate WHERE username = %s AND event.StartDate >= %s AND event.EndDate <= %s AND event.Name = %s) " \
+            "UNION (SELECT Date, '' AS VisitEventName, Name AS SiteName, '0' AS Price from site join visit_site on visit_site.SiteName = site.Name) " \
+            "WHERE username = %s AND StartDate)"
+            cursor.execute(visit_history, (username, startdate, enddate, eventname, username))
+            visit_history = cursor.fetchall()
+
+            sites = "SELECT site.Name from site"
+            cursor.execute(sites)
+            sites = cursor.fetchall()
+
+        return render_template('visit/visit_history.html', data=visit_history, site = sites)
