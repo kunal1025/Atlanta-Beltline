f46664e0fbeb33c1d09efea51807043d4fa94df7
Patel, Parth K <ppatel396@gatech.edu>
2019-04-20 13:36:58 -0400

    did site detail

--- sites.py
+++ sites.py
@@ -6,6 +6,19 @@ import db
 
 bp = Blueprint('sites', __name__, url_prefix='/site')
 
-@bp.route('/', methods=['GET'])
-def idk():
-    return redirect('/')
+@bp.route('/detail/<Name>', methods=['GET'])
+def detail(Name):
+    conn = db.get_connection()
+    if request.method == 'GET':
+        with conn.cursor() as cursor:
+            getALLSITES = "SELECT Name as site, OpenEveryDay as openEveryday, concat(Address, " ", Zipcode) as address FROM beltline.site"
+            cursor.execute(getALLSITES)
+            sites = cursor.fetchone()
+    else:
+        with conn.cursor() as cursor:
+            sitedate = "INSERT into visit_site (%s, %s, %s)"
+            cursor.execute(sitedate, (Username, SiteName, Date))
+            conn.commit()
+            sitedate = cursor.fetchone()
+
+    return redirect('sites/site_detail.html', data=sites)
