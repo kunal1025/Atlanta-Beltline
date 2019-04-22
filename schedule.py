eeba303eb462c0a103ce5df65c975ffde82a5e15
Patel, Jay Bipin <jay.patel@gatech.edu>
2019-04-21 22:50:03 -0400

    view_sched

--- schedule.py
+++ schedule.py
@@ -36,11 +36,12 @@ def view():
 
 				getEvent = 'SELECT Name as eventName, SiteName as siteName, event.StartDate as startDate, event.EndDate as endDate, count(Username) as staffCount from '\
 						'event JOIN assign_to using(Name, SiteName) WHERE Name like %s AND event.description like %s '\
+						'AND event.StartDate between %s AND "2100-02-09" AND event.EndDate between "2001-02-09" AND %s '\
 						'GROUP BY event.StartDate, Name, SiteName '
 							#'event.description LIKE %s ' \
 
 				#cursor.execute(getEvent, (ifnull(eventName,"%"), ifnull(startDate,"2000-01-01"), ifnull(endDate,"2100-02-09"), ifnull(desc,"%")))
-				cursor.execute(getEvent, (ifnull(eventName,"%"), ifnull(desc,"%")))
+				cursor.execute(getEvent, (ifnull(eventName,"%"), ifnull(desc,"%"), ifnull(startDate,"2000-01-01"), ifnull(endDate,"2100-02-09")))
 				event = cursor.fetchall()
 
 				return render_template('/view_schedule.html', events=event)
