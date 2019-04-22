bdfc2c5d7e555591c9dee1aecaac4d2643a2cd1a
Raj Patel <rpatel443@gatech.edu>
2019-04-21 22:49:43 -0400

    Raj

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, redirect, session, request
-import auth, register, transit, sites, event, detail, visit, schedule
+import auth, register, transit, sites, event, detail, visit, schedule, manage
 import db
 
 app = Flask(__name__)
