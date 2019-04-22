8f5b1e27438b2eb37180d1bb9bec61da8588455d
Kunal Patel <kunalp1025@gmail.com>
2019-04-22 00:27:58 -0400

    fix password hashing

--- passwordhash.py
+++ passwordhash.py
@@ -1,4 +1,4 @@
-import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 import db
 
 # from user password and username
@@ -6,14 +6,15 @@ import db
 
 def hashing():
     conn = db.get_connection()
-    sql = "SELECT Username, Password FROM user"
+    sql = "SELECT username, password FROM user"
     with conn.cursor() as cursor:
-        cursor.execute(sql, email)
+        cursor.execute(sql)
         users = cursor.fetchall()
         for user in users:
-            hashedpass = generate_password_hash(user.Password)
-            new = "UPDATE user set password = %s WHERE user = %s"
-            cursor.execute(new, (hashedpass, user))
+            hashedpass = generate_password_hash(user['password'])
+            new = "UPDATE user set password = %s WHERE username = %s"
+            cursor.execute(new, (hashedpass, user['username']))
             conn.commit()
 
 hashing()
+print('done')
